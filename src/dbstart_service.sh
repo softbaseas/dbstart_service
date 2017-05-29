cc61a5283ea83a6df19f8e93a8d6a8d31381bccc
Anders W. Olsen <anders@wiberg.tech>
2017-05-29 11:22:56 +0200

    esac

--- src/dbstart_service.sh
+++ src/dbstart_service.sh
@@ -26,7 +26,8 @@ if [ -f "/usr/lib/systemd/system/oracle-rdbms.service" ]; then
   read userchoice
   case "$userchoice" in
     "y") rm /usr/lib/systemd/system/oracle-rdbms.service; if [ -f "/etc/systemd/system/oracle-rdbms.service" ]; then rm /etc/systemd/system/oracle-rdbms.service fi ;
-    *) ;
+    *) ;;
+  esac
 
 echo
 
