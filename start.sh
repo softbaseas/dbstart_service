59717bb9b7fefa83502573fed8da070e410c254e
Anders W. Olsen <anders@wiberg.tech>
2017-05-29 11:07:54 +0200

    check for updates

--- start.sh
+++ start.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-git pull
+git fetch origin
+if [[ $(git diff origin/master) ]]; then
+  echo "There are updates."
+  echo "Updating..."
+  git pull -q
+  echo "Project has been updated."
+  echo
+else
+  echo "Project already up-to-date."
+fi
+
 ./src/dbstart_service.sh
