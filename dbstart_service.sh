5058eeb420b765e8a88b39d79a2675376a878d62
Anders W. Olsen <anders@wiberg.tech>
2017-05-29 09:58:54 +0200

    commented out stuff

--- dbstart_service.sh
+++ dbstart_service.sh
@@ -5,11 +5,11 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 if [ `ps -e|grep " 1 ?"|cut -d " " -f15` != "systemd" ]; then
-  echo "Systemd is not present. Use init.d scripts instead!"
+  echo "Systemd is not present. Use init.d scripts instead."
   exit 1
 fi
 
-echo "List of existing Oracle Homes:"
-echo "------------------------------"
-cat `cat /etc/oraInst.loc|grep inventory_loc|cut -d '=' -f2` /ContentsXML/inventory.xml|grep "HOME NAME"|cut -d `"` -f 4
-echo
+#echo "List of existing Oracle Homes:"
+#echo "------------------------------"
+#cat `cat /etc/oraInst.loc|grep inventory_loc|cut -d '=' -f2` /ContentsXML/inventory.xml|grep "HOME NAME"|cut -d `"` -f 4
+#echo
