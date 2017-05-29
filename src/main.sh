17dd92b95ddd27b7ca6f5e197a33aa43e4ec4cdf
Anders W. Olsen <anders@wiberg.tech>
2017-05-29 12:49:16 +0200

    creating menu

--- src/main.sh
+++ src/main.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+echo "Choose one of the following possibilities:"
+echo " 1 - Create the dbstart service."
+echo " 2 - Create the webnm service."
+echo " q - Exit"
+
+while true; do
+  read -p "Choice: " choice
+  case $choice in
+    [1]*)  ./src/dbstart_service.sh ;;
+    [2]*) ./src/webnm_service.sh ;;
+    [q]*) exit 0 ;;
+  esac
+done
