43eb3c4d3c01f0f3b78b379e9ee761fe5ca66295
Anders W. Olsen <anders@wiberg.tech>
2017-05-30 12:46:08 +0200

    remain after exit

--- src/namingservice_service.sh
+++ src/namingservice_service.sh
@@ -47,6 +47,7 @@ fi
   Restart=no
   ExecStart=/home/oracle/WlsScripts/Startnamingservice.sh
   ExecStop=/home/oracle/WlsScripts/Stopnamingservice.sh
+  RemainAfterExit=yes
 
   [Install]
   WantedBy=multi-user.target" > /usr/lib/systemd/system/namingservice.service
